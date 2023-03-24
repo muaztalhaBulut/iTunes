a08e675f3fbf065bd3f5c9439cd7e1c4ae43320e
muaztalhaBulut <84229657+muaztalhaBulut@users.noreply.github.com>
2023-03-25 01:26:13 +0300

    Configure Segmented Control

--- iTunes/View/Home/HomeViewContoller.swift
+++ iTunes/View/Home/HomeViewContoller.swift
@@ -13,7 +13,7 @@ protocol SearchOutput {
 }
 
 class HomeViewContoller: UIViewController {
-
+	
 	private lazy var results: [MediaItem] = []
 	private lazy var viewModel: SearchViewModelProtocol = SearchViewModel()
 	
@@ -27,6 +27,22 @@ class HomeViewContoller: UIViewController {
 		let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
 		collectionView.backgroundColor = .white
 		return collectionView
+	}()
+	private lazy var segmentedControl: UISegmentedControl = {
+		let segmentItems = ["All", "Movies", "Musics", "Apps", "Books"]
+		let segmentedControl = UISegmentedControl(items: segmentItems)
+		segmentedControl.selectedSegmentIndex = 0
+		segmentedControl.addTarget(self, action: #selector(segmentedControl(_:)), for: .valueChanged)
+		segmentedControl.selectedSegmentTintColor = .black
+		segmentedControl.setTitleTextAttributes([
+			NSAttributedString.Key.foregroundColor: UIColor.white,
+			NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12, weight: .semibold)
+		], for: .selected)
+		segmentedControl.setTitleTextAttributes([
+			NSAttributedString.Key.foregroundColor: UIColor.black,
+			NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12, weight: .medium)
+		], for: .normal)
+		return segmentedControl
 	}()
 	
 	private lazy var searchBar: UISearchBar = {
@@ -43,6 +59,7 @@ class HomeViewContoller: UIViewController {
 		title = "iTunes Search"
 		navigationController?.navigationBar.isHidden = false
 		setupSearchBar()
+		setSegment()
 		setupCollectionView()
 		viewModel.setDelegate(output: self)
 		viewModel.fetchData(searchBar.text ?? "")
@@ -53,12 +70,27 @@ class HomeViewContoller: UIViewController {
 		searchBar.translatesAutoresizingMaskIntoConstraints = false
 		searchBar.setConstaints(
 			top: view.safeAreaLayoutGuide.topAnchor,
-			leading: view.safeAreaLayoutGuide.leadingAnchor,
-			trailing: view.safeAreaLayoutGuide.trailingAnchor,
+			leading: view.leadingAnchor,
+			trailing: view.trailingAnchor,
+			topConstraint: 24,
+			leadingConstraint: 16,
+			trailingConstraint: 16,
 			height: 44
 		)
 	}
-	
+	private func setSegment() {
+		view.addSubview(segmentedControl)
+		segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+		segmentedControl.setConstaints(
+			top: searchBar.bottomAnchor,
+			leading: view.leadingAnchor,
+			trailing: view.trailingAnchor,
+			topConstraint: 10,
+			leadingConstraint: 24,
+			trailingConstraint: 24,
+			height: 32
+		)
+	}
 	private func setupCollectionView() {
 		view.addSubview(collectionView)
 		collectionView.delegate = self
@@ -66,12 +98,23 @@ class HomeViewContoller: UIViewController {
 		collectionView.register(SearchCollectionViewCell.self, forCellWithReuseIdentifier: SearchCollectionViewCell.Identifier.custom.rawValue)
 		collectionView.translatesAutoresizingMaskIntoConstraints = false
 		collectionView.setConstaints(
-			top: searchBar.bottomAnchor,
-			leading: view.safeAreaLayoutGuide.leadingAnchor,
-			bottom: view.safeAreaLayoutGuide.bottomAnchor,
-			trailing: view.safeAreaLayoutGuide.trailingAnchor)
-		}
+			top: segmentedControl.bottomAnchor,
+			leading: view.leadingAnchor,
+			bottom: view.bottomAnchor,
+			trailing: view.trailingAnchor,
+			topConstraint: 16,
+			leadingConstraint: .zero,
+			bottomConstraint: .zero,
+			trailingConstraint: .zero)
 	}
+	@objc
+	func segmentedControl(_ sender: UISegmentedControl) {
+		let selectedSegmentIndex = sender.selectedSegmentIndex
+		guard let mediaType = MediaType(segment: selectedSegmentIndex) else { return }
+		
+		collectionView.scrollToItem(at: .init(row: .zero, section: .zero), at: .centeredVertically, animated: true)
+	}
+}
 
 extension HomeViewContoller: UICollectionViewDataSource {
 	func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -103,17 +146,14 @@ extension HomeViewContoller: SearchOutput {
 	func saveDatas(values: [MediaItem]) {
 		results = values
 		DispatchQueue.main.async {
-			   self.collectionView.reloadData()
-		   }
+			self.collectionView.reloadData()
+		}
 	}
-	
-	
 }
 
 extension HomeViewContoller: UISearchBarDelegate {
 	func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
 		searchBar.resignFirstResponder()
 		viewModel.fetchData(searchBar.text ?? "")
-		//print("Search: \(searchBar.text ?? "")")
 	}
 }
